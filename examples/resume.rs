915029af979bfe386515674cc222805230e4e81d
nibon7 <nibon7@163.com>
2021-06-06 23:03:40 +0800

    impl CVPreamble for latex::Preamble
    Signed-off-by: nibon7 <nibon7@163.com>

--- examples/resume.rs
+++ examples/resume.rs
@@ -1,19 +1,19 @@
 use latex::{Document, DocumentClass, Element};
-use moderncv::*;
+use moderncv::CVPreamble;
 
 fn main() {
     let mut doc = Document::new(DocumentClass::Other("moderncv".to_string()));
-    doc.preamble.push(moderncvtheme("casual", Some("green")));
-    doc.preamble.push(firstname("John"));
-    doc.preamble.push(familyname("Doe"));
+    doc.preamble.theme("casual", Some("green"));
+    doc.preamble.firstname("John");
+    doc.preamble.familyname("Doe");
     doc.preamble.title("Design enthusiast");
     doc.preamble
-        .push(address("12 somestreet", Some("3456 somecity"), None));
-    doc.preamble.push(mobile("+123 456 7890"));
-    doc.preamble.push(phone("+12 (3)456 78 90"));
-    doc.preamble.push(email("jdoe@design.org"));
-    doc.preamble.push(photo("jdoe_picture", Some("64pt")));
-    doc.preamble.push(quote("Any intelligent fool can make things bigger, more complex, and more violent. It takes a touch of genius -- and a lot of courage -- to move in the opposite direction"));
+        .address("12 somestreet", Some("3456 somecity"), None);
+    doc.preamble.mobile("+123 456 7890");
+    doc.preamble.phone("+12 (3)456 78 90");
+    doc.preamble.email("jdoe@design.org");
+    doc.preamble.photo("jdoe_picture", Some("64pt"), None);
+    doc.preamble.quote("Any intelligent fool can make things bigger, more complex, and more violent. It takes a touch of genius -- and a lot of courage -- to move in the opposite direction");
 
     doc.push(Element::TitlePage);
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,92 +1,193 @@
 //! An ergonomic library for programatically generating LaTeX resume.
 
-use latex::PreambleElement;
+use latex::{Preamble, PreambleElement};
 
-macro_rules! moderncv {
-    ($fun:ident, {$arg:ident}, $ret:ty, $doc:literal) => {
-        #[doc = $doc]
-        pub fn $fun<S: AsRef<str>>($arg: S) -> $ret {
-            <$ret>::UserDefined(format!(r"\{}{{{}}}", stringify!($fun), $arg.as_ref()))
-        }
-    };
+macro_rules! texify {
+    ($name:expr $(,[$opt:ident])* $(,$extra:ident)+) => {{
+        let mut s = format!(r"\{}", $name);
 
-    ($fun:ident, {$arg:ident}, {$extra:ident}, $ret:ty, $doc:literal) => {
-        #[doc = $doc]
-        pub fn $fun<S: AsRef<str>>($arg: S, $extra: S) -> $ret {
-            <$ret>::UserDefined(format!(
-                r"\{}{{{}}}{{{}}}",
-                stringify!($fun),
-                $arg.as_ref(),
-                $extra.as_ref()
-            ))
-        }
-    };
-
-    ($fun:ident, {$arg:ident}, {$extra1:ident}, {$extra2:ident}, $ret:ty, $doc:literal) => {
-        #[doc = $doc]
-        pub fn $fun<S: AsRef<str>>($arg: S, $extra1: Option<S>, $extra2: Option<S>) -> $ret {
-            let mut s = format!(r"\{}{{{}}}", stringify!($fun), $arg.as_ref());
-            match $extra1 {
-                Some(extra) => s.push_str(&format!(r"{{{}}}", extra.as_ref())),
-                None => return <$ret>::UserDefined(s),
+        $(
+            if let Some(opt) = $opt {
+                s.push('[');
+                s.push_str(opt);
+                s.push(']');
             }
+        )*
 
-            if let Some(extra) = $extra2 {
-                s.push_str(&format!(r"{{{}}}", extra.as_ref()));
-            }
-
-            <$ret>::UserDefined(s)
-        }
-    };
-
-    ($fun:ident, [$opt:ident], {$arg:ident}, $ret:ty, $doc:literal) => {
-        #[doc = $doc]
-        pub fn $fun<S: AsRef<str>>($arg: S, $opt: Option<S>) -> $ret {
-            let s = match $opt {
-                Some(opt) => format!(
-                    r"\{}[{}]{{{}}}",
-                    stringify!($fun),
-                    opt.as_ref(),
-                    $arg.as_ref()
-                ),
-                None => format!(r"\{}{{{}}}", stringify!($fun), $arg.as_ref()),
-            };
-
-            <$ret>::UserDefined(s)
-        }
-    };
+        $(
+            s.push('{');
+            s.push_str($extra);
+            s.push('}');
+        )+
+            s
+    }};
 }
 
-moderncv!(name, { fistname }, { lastname }, PreambleElement, "Name");
-moderncv!(firstname, { firstname }, PreambleElement, "First name");
-moderncv!(familyname, { familyname }, PreambleElement, "Family name");
-moderncv!(
-    address,
-    { street },
-    { city },
-    { country },
-    PreambleElement,
-    "Address"
-);
-moderncv!(mobile, { mobile }, PreambleElement, "Mobile");
-moderncv!(phone, { phone }, PreambleElement, "Phone");
-moderncv!(fax, { fax }, PreambleElement, "Fax");
-moderncv!(email, { email }, PreambleElement, "Email");
-moderncv!(
-    moderncvtheme,
-    [opt],
-    { theme },
-    PreambleElement,
-    "Moderncv theme"
-);
-moderncv!(
-    extrainfo,
-    { extrainfo },
-    PreambleElement,
-    "Extra infomation"
-);
-moderncv!(photo, [opt], { photo }, PreambleElement, "Photo");
-moderncv!(quote, { quote }, PreambleElement, "Quote");
+/// A trait which implement useful functions for moderncv preamble
+pub trait CVPreamble {
+    fn name(&mut self, firstname: &str, lastname: &str) -> &mut Self;
+    fn firstname(&mut self, fistname: &str) -> &mut Self;
+    fn lastname(&mut self, lastname: &str) -> &mut Self;
+    fn givenname(&mut self, givenname: &str) -> &mut Self;
+    fn familyname(&mut self, familyname: &str) -> &mut Self;
+    fn address(&mut self, street: &str, city: Option<&str>, country: Option<&str>) -> &mut Self;
+    fn mobile(&mut self, mobile: &str) -> &mut Self;
+    fn phone(&mut self, phone: &str) -> &mut Self;
+    fn fax(&mut self, fax: &str) -> &mut Self;
+    fn email(&mut self, email: &str) -> &mut Self;
+    fn theme(&mut self, theme: &str, opt: Option<&str>) -> &mut Self;
+    fn extrainfo(&mut self, info: &str) -> &mut Self;
+    fn photo(&mut self, photo: &str, width: Option<&str>, frame: Option<&str>) -> &mut Self;
+    fn quote(&mut self, quote: &str) -> &mut Self;
+}
+
+impl CVPreamble for Preamble {
+    /// Set name
+    fn name(&mut self, firstname: &str, lastname: &str) -> &mut Self {
+        let s = texify!("name", firstname, lastname);
+        let elem = PreambleElement::UserDefined(s);
+        self.push(elem);
+
+        self
+    }
+
+    /// Set first name
+    fn firstname(&mut self, firstname: &str) -> &mut Self {
+        let s = texify!("firstname", firstname);
+        let elem = PreambleElement::UserDefined(s);
+        self.push(elem);
+
+        self
+    }
+
+    /// Set last name
+    fn lastname(&mut self, lastname: &str) -> &mut Self {
+        let s = texify!("lastname", lastname);
+        let elem = PreambleElement::UserDefined(s);
+        self.push(elem);
+
+        self
+    }
+
+    /// Set given name (First name)
+    fn givenname(&mut self, givenname: &str) -> &mut Self {
+        let s = texify!("givenname", givenname);
+        let elem = PreambleElement::UserDefined(s);
+        self.push(elem);
+
+        self
+    }
+
+    /// Set family name (Last name)
+    fn familyname(&mut self, familyname: &str) -> &mut Self {
+        let s = texify!("familyname", familyname);
+        let elem = PreambleElement::UserDefined(s);
+        self.push(elem);
+
+        self
+    }
+
+    /// Set address
+    fn address(&mut self, street: &str, city: Option<&str>, country: Option<&str>) -> &mut Self {
+        let mut s = texify!("address", street);
+        match city {
+            Some(city) => {
+                s.push('{');
+                s.push_str(city);
+                s.push('}');
+            }
+            None => {
+                let elem = PreambleElement::UserDefined(s);
+                self.push(elem);
+
+                return self;
+            }
+        }
+
+        if let Some(country) = country {
+            s.push('{');
+            s.push_str(country);
+            s.push('}');
+        }
+
+        let elem = PreambleElement::UserDefined(s);
+        self.push(elem);
+
+        self
+    }
+
+    /// Set mobile number
+    fn mobile(&mut self, mobile: &str) -> &mut Self {
+        let s = texify!("mobile", mobile);
+        let elem = PreambleElement::UserDefined(s);
+        self.push(elem);
+
+        self
+    }
+
+    /// Set phone number
+    fn phone(&mut self, phone: &str) -> &mut Self {
+        let s = texify!("phone", phone);
+        let elem = PreambleElement::UserDefined(s);
+        self.push(elem);
+
+        self
+    }
+
+    /// Set fax number
+    fn fax(&mut self, fax: &str) -> &mut Self {
+        let s = texify!("fax", fax);
+        let elem = PreambleElement::UserDefined(s);
+        self.push(elem);
+
+        self
+    }
+
+    /// Set email address
+    fn email(&mut self, email: &str) -> &mut Self {
+        let s = texify!("email", email);
+        let elem = PreambleElement::UserDefined(s);
+        self.push(elem);
+
+        self
+    }
+
+    /// Set moderncv theme
+    fn theme(&mut self, theme: &str, opt: Option<&str>) -> &mut Self {
+        let s = texify!("moderncvtheme", [opt], theme);
+        let elem = PreambleElement::UserDefined(s);
+        self.push(elem);
+
+        self
+    }
+
+    /// Set extra information
+    fn extrainfo(&mut self, info: &str) -> &mut Self {
+        let s = texify!("extrainfo", info);
+        let elem = PreambleElement::UserDefined(s);
+        self.push(elem);
+
+        self
+    }
+
+    /// Set photo
+    fn photo(&mut self, photo: &str, width: Option<&str>, frame: Option<&str>) -> &mut Self {
+        let s = texify!("photo", [width], [frame], photo);
+        let elem = PreambleElement::UserDefined(s);
+        self.push(elem);
+
+        self
+    }
+
+    /// Set quote string
+    fn quote(&mut self, quote: &str) -> &mut Self {
+        let s = texify!("quote", quote);
+        let elem = PreambleElement::UserDefined(s);
+        self.push(elem);
+
+        self
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -94,108 +195,158 @@ mod tests {
 
     #[test]
     fn test_name() {
-        assert_eq!(
-            name("John", "Doe"),
-            PreambleElement::UserDefined(r"\name{John}{Doe}".to_string())
-        );
+        let mut preamble = Preamble::default();
+        preamble.name("John", "Doe");
+
+        let left = preamble.iter().nth(0).unwrap();
+        let right = PreambleElement::UserDefined(r"\name{John}{Doe}".to_string());
+        assert!(left.eq(&right));
     }
 
     #[test]
     fn test_firstname() {
-        assert_eq!(
-            firstname("John"),
-            PreambleElement::UserDefined(r"\firstname{John}".to_string())
-        );
+        let mut preamble = Preamble::default();
+        preamble.firstname("John");
+
+        let left = preamble.iter().nth(0).unwrap();
+        let right = PreambleElement::UserDefined(r"\firstname{John}".to_string());
+        assert!(left.eq(&right));
+    }
+
+    #[test]
+    fn test_lastname() {
+        let mut preamble = Preamble::default();
+        preamble.lastname("Doe");
+
+        let left = preamble.iter().nth(0).unwrap();
+        let right = PreambleElement::UserDefined(r"\lastname{Doe}".to_string());
+        assert!(left.eq(&right));
+    }
+
+    #[test]
+    fn test_givenname() {
+        let mut preamble = Preamble::default();
+        preamble.givenname("John");
+
+        let left = preamble.iter().nth(0).unwrap();
+        let right = PreambleElement::UserDefined(r"\givenname{John}".to_string());
+        assert!(left.eq(&right));
     }
 
     #[test]
     fn test_familyname() {
-        assert_eq!(
-            familyname("Doe"),
-            PreambleElement::UserDefined(r"\familyname{Doe}".to_string())
-        );
+        let mut preamble = Preamble::default();
+        preamble.familyname("Doe");
+
+        let left = preamble.iter().nth(0).unwrap();
+        let right = PreambleElement::UserDefined(r"\familyname{Doe}".to_string());
+        assert!(left.eq(&right));
     }
 
     #[test]
     fn test_address() {
-        assert_eq!(
-            address("12 somestreet", Some("3456 somecity"), None),
-            PreambleElement::UserDefined(r"\address{12 somestreet}{3456 somecity}".to_string())
-        );
+        let mut preamble = Preamble::default();
+        preamble.address("12 somestreet", Some("3456 somecity"), None);
 
-        assert_eq!(
-            address("12 somestreet", None, Some("unused")),
-            PreambleElement::UserDefined(r"\address{12 somestreet}".to_string())
-        );
+        let left = preamble.iter().nth(0).unwrap();
+        let right =
+            PreambleElement::UserDefined(r"\address{12 somestreet}{3456 somecity}".to_string());
+        assert!(left.eq(&right));
+
+        let mut preamble = Preamble::default();
+        preamble.address("12 somestreet", None, Some("unused"));
+
+        let left = preamble.iter().nth(0).unwrap();
+        let right = PreambleElement::UserDefined(r"\address{12 somestreet}".to_string());
+        assert!(left.eq(&right));
     }
 
     #[test]
     fn test_mobile() {
-        assert_eq!(
-            mobile("+123 456 7890"),
-            PreambleElement::UserDefined(r"\mobile{+123 456 7890}".to_string())
-        );
+        let mut preamble = Preamble::default();
+        preamble.mobile("+123 456 7890");
+
+        let left = preamble.iter().nth(0).unwrap();
+        let right = PreambleElement::UserDefined(r"\mobile{+123 456 7890}".to_string());
+        assert!(left.eq(&right));
     }
 
     #[test]
     fn test_phone() {
-        assert_eq!(
-            phone("12 (3)456 78 90"),
-            PreambleElement::UserDefined(r"\phone{12 (3)456 78 90}".to_string())
-        );
+        let mut preamble = Preamble::default();
+        preamble.phone("12 (3)456 78 90");
+
+        let left = preamble.iter().nth(0).unwrap();
+        let right = PreambleElement::UserDefined(r"\phone{12 (3)456 78 90}".to_string());
+        assert!(left.eq(&right));
     }
 
     #[test]
     fn test_fax() {
-        assert_eq!(
-            fax("12 (3)456 78 90"),
-            PreambleElement::UserDefined(r"\fax{12 (3)456 78 90}".to_string())
-        );
+        let mut preamble = Preamble::default();
+        preamble.fax("12 (3)456 78 90");
+
+        let left = preamble.iter().nth(0).unwrap();
+        let right = PreambleElement::UserDefined(r"\fax{12 (3)456 78 90}".to_string());
+        assert!(left.eq(&right));
     }
 
     #[test]
     fn test_email() {
-        assert_eq!(
-            email("jdoe@design.org"),
-            PreambleElement::UserDefined(r"\email{jdoe@design.org}".to_string())
-        );
+        let mut preamble = Preamble::default();
+        preamble.email("jdoe@design.org");
+
+        let left = preamble.iter().nth(0).unwrap();
+        let right = PreambleElement::UserDefined(r"\email{jdoe@design.org}".to_string());
+        assert!(left.eq(&right));
     }
 
     #[test]
-    fn test_moderncvtheme() {
-        assert_eq!(
-            moderncvtheme("classic", None),
-            PreambleElement::UserDefined(r"\moderncvtheme{classic}".to_string())
-        );
+    fn test_theme() {
+        let mut preamble = Preamble::default();
+        preamble.theme("classic", None);
 
-        assert_eq!(
-            moderncvtheme("casual", Some("green")),
-            PreambleElement::UserDefined(r"\moderncvtheme[green]{casual}".to_string())
-        );
+        let left = preamble.iter().nth(0).unwrap();
+        let right = PreambleElement::UserDefined(r"\moderncvtheme{classic}".to_string());
+        assert!(left.eq(&right));
+
+        let mut preamble = Preamble::default();
+        preamble.theme("casual", Some("green"));
+
+        let left = preamble.iter().nth(0).unwrap();
+        let right = PreambleElement::UserDefined(r"\moderncvtheme[green]{casual}".to_string());
+        assert!(left.eq(&right));
     }
 
     #[test]
     fn test_extrainfo() {
-        assert_eq!(
-            extrainfo(r"\weblink{www.ctan.org}"),
-            PreambleElement::UserDefined(r"\extrainfo{\weblink{www.ctan.org}}".to_string())
-        );
+        let mut preamble = Preamble::default();
+        preamble.extrainfo(r"\weblink{www.ctan.org}");
+
+        let left = preamble.iter().nth(0).unwrap();
+        let right = PreambleElement::UserDefined(r"\extrainfo{\weblink{www.ctan.org}}".to_string());
+        assert!(left.eq(&right));
     }
 
     #[test]
     fn test_photo() {
-        assert_eq!(
-            photo("jdoe_picture", Some("64pt")),
-            PreambleElement::UserDefined(r"\photo[64pt]{jdoe_picture}".to_string())
-        );
+        let mut preamble = Preamble::default();
+        preamble.photo("jdoe_picture", Some("64pt"), None);
+
+        let left = preamble.iter().nth(0).unwrap();
+        let right = PreambleElement::UserDefined(r"\photo[64pt]{jdoe_picture}".to_string());
+        assert!(left.eq(&right));
     }
+
     #[test]
     fn test_quote() {
-        assert_eq!(
-            quote("Any intelligent fool can make things bigger"),
-            PreambleElement::UserDefined(
-                r"\quote{Any intelligent fool can make things bigger}".to_string()
-            )
+        let mut preamble = Preamble::default();
+        preamble.quote("Any intelligent fool can make things bigger");
+
+        let left = preamble.iter().nth(0).unwrap();
+        let right = PreambleElement::UserDefined(
+            r"\quote{Any intelligent fool can make things bigger}".to_string(),
         );
+        assert!(left.eq(&right));
     }
 }
