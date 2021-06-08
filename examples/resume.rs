7e888ff360fa25fc4fa61567eb5b29114a10f869
nibon7 <nibon7@163.com>
2021-06-08 18:40:49 +0800

    update example
    Signed-off-by: nibon7 <nibon7@163.com>

--- examples/resume.rs
+++ examples/resume.rs
@@ -1,9 +1,19 @@
 use latex::{Document, Element, Section};
 use moderncv::preamble::{Color, Style};
-use moderncv::{document_class, CVPreamble, CVSection};
+use moderncv::{closesection, document_class, texify, CVPreamble, CVSection};
 
-fn main() {
-    let mut doc = Document::new(document_class());
+fn subsection(section: &str) -> Element {
+    let s = texify!("subsection", section);
+    Element::UserDefined(s)
+}
+
+fn pagebreak() -> Element {
+    let empty = "";
+    let s = texify!("pagebreak", empty);
+    Element::UserDefined(s)
+}
+
+fn make_preamble(doc: &mut Document) {
     doc.preamble.cvtheme(Style::Casual,Some(Color::Green))
     .firstname("John")
     .familyname("Doe")
@@ -13,9 +23,9 @@ fn main() {
     .phone("+12 (3)456 78 90",None)
     .email("jdoe@design.org")
     .quote("Any intelligent fool can make things bigger, more complex, and more violent. It takes a touch of genius -- and a lot of courage -- to move in the opposite direction");
+}
 
-    doc.push(Element::TitlePage);
-
+fn make_sections(doc: &mut Document) {
     let mut education = Section::new("Education");
     education
         .cventry(
@@ -46,6 +56,7 @@ fn main() {
     doc.push(thesis);
 
     let mut experience = Section::new("Experience");
+    experience.push(subsection("Vocational"));
     experience
         .cventry(
             "year--year",
@@ -62,15 +73,18 @@ fn main() {
             "City",
             None,
             Some("Description"),
-        )
-        .cventry(
-            "year--year",
-            "Job title",
-            "Employer",
-            "City",
-            None,
-            Some(r"Description line 1\newline{}Description line 2"),
         );
+
+    experience.push(subsection("Miscellaneous"));
+
+    experience.cventry(
+        "year--year",
+        "Job title",
+        "Employer",
+        "City",
+        None,
+        Some(r"Description line 1\newline{}Description line 2"),
+    );
 
     doc.push(experience);
 
@@ -87,6 +101,40 @@ fn main() {
         .cvcomputer("category 2", "XXX, YYY, ZZZ", "category 4", "XXX, YYY, ZZZ");
 
     doc.push(computer);
+
+    let mut interests = Section::new("Interests");
+    interests
+        .cvline("hobby 1", r"\small Description")
+        .cvline("hobby 2", r"\small Description")
+        .cvline("hobby 3", r"\small Description");
+
+    doc.push(interests);
+    doc.push(closesection());
+    doc.push(pagebreak());
+
+    let mut extra = Section::new("Extra");
+    extra
+        .cvlistitem("Item 1")
+        .cvlistitem("Item 2")
+        .cvlistitem("Item 3");
+
+    doc.push(extra);
+
+    let mut extra2 = Section::new("Extra 2");
+    extra2
+        .cvlistdoubleitem("Item 1", "Item 4")
+        .cvlistdoubleitem("Item 2", "Item 5")
+        .cvlistdoubleitem("Item 3", "");
+
+    doc.push(extra2);
+}
+
+fn main() {
+    let mut doc = Document::new(document_class());
+
+    make_preamble(&mut doc);
+    doc.push(Element::TitlePage);
+    make_sections(&mut doc);
 
     println!("{}", latex::print(&doc).unwrap());
 }
